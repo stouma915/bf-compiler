ecbbc8ac7318ccbf6e607a26846277f3f9647309
stouma915 <sasakitouma@yahoo.ne.jp>
2022-10-19 01:02:49 +0900

    Create result.h

---
+++ src/result.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+class Result {
+	public:
+		std::string output;
+		bool has_error;
+
+		Result(std::string _output, bool _has_error) {
+			output = _output;
+			has_error = _has_error;
+		}
+};
+
