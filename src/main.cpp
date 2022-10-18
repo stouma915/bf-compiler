2db202bbf547f62fd393184a7a064fec6a60569b
stouma915 <sasakitouma@yahoo.ne.jp>
2022-10-19 00:56:01 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main() {
-  using namespace std;
+int main(int argc, char* argv[]) {
+	using namespace std;
 
-  cout << "aaa" << endl;
+	if (argc < 2) {
+ 		cout << "no input." << endl;
+
+    		return 1;
+  	}
+
+  	ifstream source_file;
+  	source_file.open(argv[1], ios::in);
+  	if (source_file.fail()) {
+    		cout << "failed to open input file." << endl;
+		
+		return 1;
+  	}
+
+	stringstream source_stream;
+	source_stream << source_file.rdbuf();
+	source_file.close();
+
+	string source = source_stream.str();
+
+	cout << source << endl;
   
-  return 0;
+ 	return 0;
 }
