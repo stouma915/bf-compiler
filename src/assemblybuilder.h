a754300a072432409426859ba539a9e52e155e15
stouma915 <sasakitouma@yahoo.ne.jp>
2022-10-19 01:00:37 +0900

    Create assemblybuilder.h

---
+++ src/assemblybuilder.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+
+class AssemblyBuilder {
+	public:
+		std::string content;
+
+		AssemblyBuilder(std::string _content) {
+			content = _content;
+		}
+
+		void append(std::string str) {
+			content += str;
+		}
+
+		void append_line(std::string str) {
+			content += str + "\n";
+		}
+
+		std::string as_str() {
+			return content;
+		}
+};
+
