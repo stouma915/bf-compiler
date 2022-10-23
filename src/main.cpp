fe5cfc0b776c4b2c38daf3e19088bde838008fa5
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-10-23 23:42:41 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "compile.h"
 #include "util.h"
 
-#define VERSION "1.0.0"
+#define VERSION "1.0.1"
 
 void print_usage(std::string file_name) {
     std::cout << "bf-compiler v" << VERSION << "\n"
