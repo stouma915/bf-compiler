c67e378f06619a9f29fdd9d82fd5110c20f8a394
stouma915 <sasakitouma@yahoo.ne.jp>
2022-10-31 19:23:31 -0400

    specify output file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <sstream>
 
 #include "normalcompiler.h"
@@ -14,7 +15,8 @@ void print_usage(std::string file_name) {
                  "   " << file_name << " [FLAGS] [SOURCE]\n"
                  "\n"
                  "Flags:\n"
-                 "   -h, --help    prints help information.\n"
+                 "   -h, --help    print help information.\n"
+                 "   -o, --out     specify output file.\n"
                  "\n"
                  "Args:\n"
                  "   <SOURCE>      Brainf**k source file.\n";
@@ -29,19 +31,36 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    string source_file_name;
+    optional<string> source_file_opt = nullopt;
+    optional<string> output_file_opt = nullopt;
+
+    bool continue_once = false;
     for (int i = 1; i < argc; i ++) {
+        if (continue_once) {
+            continue_once = false;
+
+            continue;
+        }
+
         string arg = argv[i];
 
         if (starts_with(arg, "-")) {
-            if (arg == "-h") {
+            if (arg == "-h" || arg == "--help") {
                 print_usage(argv[0]);
 
                 return 0;
-            } else if (arg == "--help") {
-                print_usage(argv[0]);
+            } else if (arg == "-o" || arg == "--out") {
+                if (argc <= (i + 1)) {
+                    cout << "Invalid usage.\n"
+                            "\n"
+                            "For more information try --help\n";
 
-                return 0;
+                    return 1;
+                }
+
+                output_file_opt = argv[i + 1];
+
+                continue_once = true;
             } else {
                 cout << "Found argument '" << arg << "' which wasn't expected, or isn't valid in this context.\n"
                         "\n"
@@ -53,9 +72,17 @@ int main(int argc, char* argv[]) {
                 return 1;
             }
         } else {
-            source_file_name = arg;
+            source_file_opt = arg;
         }
     }
+
+    if (source_file_opt == nullopt) {
+        print_usage(argv[0]);
+
+        return 1;
+    }
+
+    string source_file_name = source_file_opt.value();
 
     ifstream source_file;
     source_file.open(source_file_name, ios::in);
@@ -78,7 +105,23 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    cout << rs.output;
+    if (output_file_opt != nullopt) {
+        string output_file_name = output_file_opt.value();
+
+        ofstream output_file;
+        output_file.open(output_file_name);
+        if (output_file.fail()) {
+            cout << "Failed to create '" << output_file_name << "'." << endl;
+
+            return 1;
+        }
+
+        output_file << rs.output;
+
+        output_file.close();
+    } else {
+        cout << rs.output;
+    }
   
     return 0;
 }
